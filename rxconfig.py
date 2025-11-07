14ec5415fbf65236b9efbcd2003411c8d3ad66b4
Meshell <meshelleva@gmail.com>
2025-11-07 13:57:04 +0100

    configure the the server port

--- rxconfig.py
+++ rxconfig.py
@@ -1,11 +1,10 @@
-import os
+
 import reflex as rx
 
 config = rx.Config(
     app_name="portfolio",
     show_built_with_reflex = False,
-    frontend_port=int(os.getenv("PORT", 3000)),  
-    # backend_port=8000  # Optional: if you're exposing backend separately
+    api_url="http://0.0.0.0:1000",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(), 
